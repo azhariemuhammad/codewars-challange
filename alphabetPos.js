64b59cf7ad51f0c436fa2aec3c92ca924ed45195
dewisartikae <dewiesartikae@gmail.com>
2018-07-04 00:28:59 +0900

    my soution

--- alphabetPos.js
+++ alphabetPos.js
@@ -11,9 +11,20 @@
 // alphabet_position("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" as a string.
 
-function alphabetPosition() {
-
-}
-
-
-alphabet_position("The sunset sets at twelve o' clock.") // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+// solution 
+function alphabetPosition(text) {
+    text = text.toLowerCase();
+    let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+    "s", "t", "u", "v", "w", "x", "y", "z"];
+    let newArr = text.split("");
+    let result = [];
+    for (let i = 0; i < newArr.length; i++) {
+        if (alphabet.indexOf(newArr[i]) != -1) {
+            result.push(alphabet.indexOf(newArr[i]) + 1);
+        }
+    }
+    //console.log(result);
+    return result.join(" ");
+  }
+  
+  console.log(alphabetPosition("The sunset sets at twelve o' clock"));
