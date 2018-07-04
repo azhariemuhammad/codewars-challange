7ffdd2372eeea158390b443f9dddf1363d889671
Dewi Sartika M <38138009+dewisartikae@users.noreply.github.com>
2018-07-04 22:16:06 +0900

    Update uniqueInOrder.js

--- uniqueInOrder.js
+++ uniqueInOrder.js
@@ -25,7 +25,3 @@ function uniqueInOrder (items) {
 console.log(uniqueInOrder('AAAABBBCCDAABBB')); // == ['A', 'B', 'C', 'D', 'A', 'B']
 console.log(uniqueInOrder('ABBCcAD'));         // == ['A', 'B', 'C', 'c', 'A', 'D']
 console.log(uniqueInOrder([1,2,2,3,3]));       // == [1,2,3]
-
-uniqueInOrder('AAAABBBCCDAABBB') // == ['A', 'B', 'C', 'D', 'A', 'B']
-uniqueInOrder('ABBCcAD')         // == ['A', 'B', 'C', 'c', 'A', 'D']
-uniqueInOrder([1,2,2,3,3])       // == [1,2,3]
